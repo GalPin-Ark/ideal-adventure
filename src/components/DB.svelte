65f865a064d09b73e65a268a00f0a80756109b34
unozerocode <enrique@unozerocode.com>
2020-01-27 10:56:26 -0600

    Initial commit

---
+++ src/components/DB.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { draw } from 'svelte/transition';
+	import { bounce } from 'svelte/easing';
+</script>
+
+<svg viewBox="0 0 5 5" xmlns="http://www.w3.org/2000/svg">
+	{#if true}
+		<path transition:draw="{{duration: 5000, delay: 500, easing: bounce}}"
+					d="M2 1 h1 v1 h1 v1 h-1 v1 h-1 v-1 h-1 v-1 h1 z"
+					fill="none"
+					stroke="red"
+					stroke-width="0.1px"
+					stroke-linejoin="round"
+		/>
+        <circle cx="50%" cy="50%" r="0.5" fill="cornflowerblue"/>
+	{/if}
+</svg>
