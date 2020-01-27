d3f92c3100b8416126276d85e25f186579637294
unozerocode <enrique@unozerocode.com>
2020-01-27 11:01:31 -0600

    Fix bounce transition

--- src/components/DB.svelte
+++ src/components/DB.svelte
@@ -1,11 +1,11 @@
 <script>
 	import { draw } from 'svelte/transition';
-	import { bounce } from 'svelte/easing';
+	import { bounceIn } from 'svelte/easing';
 </script>
 
 <svg viewBox="0 0 5 5" xmlns="http://www.w3.org/2000/svg">
 	{#if true}
-		<path transition:draw="{{duration: 5000, delay: 500, easing: bounce}}"
+		<path transition:draw="{{duration: 5000, delay: 500, easing: bounceIn}}"
 					d="M2 1 h1 v1 h1 v1 h-1 v1 h-1 v-1 h-1 v-1 h1 z"
 					fill="none"
 					stroke="red"
